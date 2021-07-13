d53c59e6bc9b824ff2e3c435631c1b7572e81c78
Anujaudayan <82448663+Anujaudayan@users.noreply.github.com>
2021-07-13 21:40:08 +0530

    Add files via upload

---
+++ Frontend/src/app/header/header.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../auth.service';
+import { Router } from '@angular/router';
+
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent implements OnInit {
+
+  loggedUser : any ='';
+  constructor(public _auth :AuthService, private _router :Router ) { }
+
+  ngOnInit(): void {
+    this.loggedUser = this._auth.getCurrentUser();
+  }
+logoutUser()
+{
+localStorage.removeItem('token')
+this._router.navigate(['/'])
+}
+
+getloguser(){
+  this.loggedUser = this._auth.getCurrentUser(); 
+}
+}
